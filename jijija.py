dff4a5b65cf634fec75dea827bc8e58e288b7a40
mrghost011-j <gsatizabalpretel@gmail.com>
2026-04-15 18:17:30 -0500

    0.2
    version 0.0.2

--- jijija.py
+++ jijija.py
@@ -1,1 +1,23 @@
-print("hola mundo") 
+def calculator():
+    num1 = float(input("Ingresa el primer número: "))
+    operation = input("Ingresa la operación (+, -, *, /): ")
+    num2 = float(input("Ingresa el segundo número: "))
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: división por cero"
+    else:
+        result = "Operación no válida"
+
+    print(f"El resultado es: {result}")
+
+calculator() 
+
